0cde53156b0dfe6ebfb65db63cf11bea49ee5ba8
Patt R <frostshoxx@gmail.com>
2019-10-16 21:16:21 -0700

    Attempt to add files in a lazy way

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+import '@fortawesome/fontawesome-free/css/all.min.css' 
+import 'bootstrap-css-only/css/bootstrap.min.css' 
+import 'mdbreact/dist/css/mdb.css'
